83d06b0e07f262541509acce7e098a4f31e22fb4
jk10s <juanka_267@hotmail.com>
2023-05-25 19:06:20 -0500

    update all

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const path = require('path');
 
 const app = express();
 app.use(express.urlencoded({
@@ -38,14 +39,77 @@ app.post('/registrar', (req, res) => {
     });
 });
 
+// Ruta para el inicio de sesión
+app.get('/sesion', (req, res) => {
+    res.render('sesion', {
+        mensajeError: null
+    });
+});
+
+app.post('/iniciar-sesion', (req, res) => {
+    const {
+        email,
+        password
+    } = req.body;
+
+    const query = 'SELECT * FROM usuarios WHERE email = ? AND password = ?';
+    const values = [email, password];
+
+    connection.query(query, values, (error, results) => {
+        if (error) {
+            console.error('Error al realizar la consulta:', error);
+            res.render('sesion', {
+                mensajeError: 'Error al iniciar sesión.'
+            });
+        } else {
+            if (results.length > 0) {
+                // El usuario ha iniciado sesión correctamente
+                // Puedes redirigirlo a la página principal u otra página de tu elección
+                res.redirect('/'); // Ejemplo: redirige a la página principal
+            } else {
+                // Las credenciales son incorrectas
+                res.render('sesion', {
+                    mensajeError: 'Credenciales incorrectas.'
+                });
+            }
+        }
+    });
+});
+
+
+// Configuración de la carpeta de archivos estáticos
+app.use(express.static(path.join(__dirname, 'assets')));
+
 // Ruta para la página principal
 app.get('/', (req, res) => {
-    res.send('¡Bienvenido a la plataforma de registro de usuarios!');
+    res.render('index');
+});
+// Ruta para mostrar la página de registro
+app.get('/registrarse.html', (req, res) => {
+    res.render('registrarse'); // Renderiza el archivo "registrarse.ejs"
+});
+app.get('/courses.html', (req, res) => {
+    res.render('courses');
+});
+app.get('/about.html', (req, res) => {
+    res.render('about');
+});
+
+app.get('/sesion.html', (req, res) => {
+    res.render('sesion');
+});
+
+app.get('/course-details.html', (req, res) => {
+    res.render('course-details');
 });
 
 app.get('/usuarios.html', (req, res) => {
-    res.sendFile(__dirname + '/usuarios.html');
+    res.render('usuarios');
 });
+
+// Configuración del motor de plantillas EJS
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Iniciar el servidor
 app.listen(3000, () => {
